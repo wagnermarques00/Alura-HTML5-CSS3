0a09342885fa2842b00c2d657f59f4b9d9bda345
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-01-08 15:41:32 -0300

    Flexbox and Grid 03/06

--- CSS -  Arranging elements with Flexbox and Grid/index.js
+++ CSS -  Arranging elements with Flexbox and Grid/index.js
@@ -2,5 +2,5 @@ const menuButton = document.querySelector('.header__menu');
 const menu = document.querySelector('.lateral-menu');
 
 menuButton.addEventListener('click', () => {
-  menu.classList.toggle('lateral-menu--active')
-})
+  menu.classList.toggle('lateral-menu--active');
+});
