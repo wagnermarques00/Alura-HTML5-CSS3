605da7eaf7417f758b0a2e1c6b7077a7f59acc09
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2023-02-03 14:20:10 -0300

    Tailwind React - 02/05

--- React with TailwindCSS - Styling Components/alura-newsletter/src/components/Header/Header.jsx
+++ React with TailwindCSS - Styling Components/alura-newsletter/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
+
 const Header = () => {
 	return (
-		<header className="flex justify-between items-center h-20 px-5 bg-gray-500 sm:rounded-xl sm:m-5">
+		<header className="flex justify-between items-center h-20 px-5 bg-alura-200 dark:bg-dark-200 sm:rounded-xl sm:m-5">
 			<span className="text-gray-100">Olá, usuário</span>
 			<h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
-			<input className="hidden sm:block" type="checkbox" />
+			<ToggleTheme />
 		</header>
 	);
 };
--- React with TailwindCSS - Styling Components/alura-newsletter/tailwind.config.cjs
+++ React with TailwindCSS - Styling Components/alura-newsletter/tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
 	content: ["./index.html", "./src/**/*.{js,ts,jsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: {
+					100: "rgb(18, 18, 18)",
+					200: "rgb(51, 51, 51)",
+				},
+				alura: {
+					100: "#167bf7",
+					200: "#051933",
+				},
+			},
+		},
 	},
 	plugins: [],
 };
